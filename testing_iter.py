572e55e0bf7b8e22aff1639be392296ae7099b87
Domnigg20 <100387265+Domnigg20@users.noreply.github.com>
2024-01-09 22:26:02 +0000

    I change comments

--- testing_iter.py
+++ testing_iter.py
@@ -1,15 +1,22 @@
 # sequence_iter.py
 #How iter, next and iteration works
 
+
+#define a list of numbers
 Y = [1,2,3,4]
+
+# The class is the iterator, which implements the __iter__
+# and __next__ method
 class SequenceIterator:
     def __init__(self, sequence):
         self._sequence = sequence
         self._index = 0
 
+#calling the iterator, which returns self
     def __iter__(self):
         return self
 
+#iterates over the next item in the list
     def __next__(self):
         if self._index < len(self._sequence):
             item = self._sequence[self._index]
@@ -18,5 +25,6 @@ class SequenceIterator:
         else:
             raise StopIteration
         
+#Parse a variable or iteraable object "obj"                                             
 obj= SequenceIterator(Y**2)
 print(obj._sequence)
